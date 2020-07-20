2bd06d2dc95ff5776e6e07265c5a97a950653776
hj252 <hj252@uvc.co.kr>
2020-07-21 08:59:11 +0900

    Fix script in main JS
    1.  Fix script "hover" in main.js
    (hover - > mouseenter, mouseleave)

--- asset/js/header_footer.js
+++ asset/js/header_footer.js
@@ -1,8 +1,11 @@
 $(document).ready(function(){
 	/*헤더에 마우스 올리면 클래스 토글*/
-    $('#header').hover(function(){
-		$(this).toggleClass("on");
+    $('#header').mouseenter(function(){
+		$(this).addClass("on");
 	});
+	$('#header').mouseleave(function(){
+		$(this).removeClass("on");
+	}); //hover기능은 로드가 전부 되지 않았을때 반대로 되는 경우가 있음
 	
 	$('#header').mouseover(function(){
 		$('.logo').attr("src","asset/images/uvc_logo_nav.png");
