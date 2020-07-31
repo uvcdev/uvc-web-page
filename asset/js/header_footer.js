f1cbc702957846850b9b101407f96890df88e0bb
hj252 <hj252@uvc.co.kr>
2020-07-31 19:27:13 +0900

    Fix header in mobie
    1. Fix header in mobile

--- asset/js/header_footer.js
+++ asset/js/header_footer.js
@@ -1,20 +1,35 @@
-$(document).ready(function(){
+let visible = false
+
+const mb = '1170'
+
+function resizeHandler() {
+	const size = document.body.getBoundingClientRect().width	
+	if(size < mb) {
+		$('.gnb').css({'display': 'none'})
+	} else {
+		$('.gnb').css({'display': 'flex'})
+	}
+}
+
+window.addEventListener('resize', resizeHandler)
+
+$(document).ready(function() {
+
 	/*헤더에 마우스 올리면 클래스 토글*/
     $('#header').mouseenter(function(){
-		$(this).addClass("on");
+		if(window.innerWidth > 1180) {
+			$(this).addClass("on");
+			$('h1 a img.color').addClass("active");
+		}
 	});
 	$('#header').mouseleave(function(){
-		$(this).removeClass("on");
+		if(window.innerWidth > 1180) {
+			$(this).removeClass("on");
+			$('h1 a img.color').removeClass("active");
+		}
 	}); //hover기능은 로드가 전부 되지 않았을때 반대로 되는 경우가 있음
+
 	
-	$('#header').mouseover(function(){
-		$('.logo').attr("src","https://github.com/uvcdev/uvc-web-page/blob/master/asset/images/uvc_logo_nav.png?raw=true");
-	});
-
-	$('#header').mouseleave(function(){
-		$('.logo').attr("src","https://github.com/uvcdev/uvc-web-page/blob/master/asset/images/uvc_logo_foot.png?raw=true");
-	})
-
     /*유틸메뉴 언어선택*/
     $('.lang').click(function(){
 		$('.lang_util ul').slideToggle(400);
@@ -22,10 +37,21 @@ $(document).ready(function(){
     
 	/*전체메뉴*/
 	$('.mNav').click(function(){
-		$('.gnb').slideToggle(400);
-		$(this).toggleClass("on");
+		const mNav = $(this)
+		console.log(mNav.name);
+		const isOn = mNav.attr('name')
+		if (isOn === 'on') {
+			$('.gnb').slideUp(400);
+			$(this).toggleClass("on");
+			mNav.attr('name', 'off')
+		} else { 
+			$('.gnb').slideDown(400);
+			$(this).toggleClass("on");
+			mNav.attr('name', 'on')
+		}
 		$('li').click(function(){
-			$('.gnb').css({"display":"none"});
+			$('.gnb').slideUp(400)
+			mNav.attr('name', 'off')
 			$('.mNav').removeClass("on");
 		});
 	});
@@ -39,11 +65,4 @@ $(document).ready(function(){
 		$('.gnb > .sol > ul').css({"visibility":"hidden", "opacity":"0"});
 	});
 
-});
-    
-$(window).resize(function(){
-	var wWidth = $(window).width();
-	if(wWidth > 1180 && $(".gnb").is(":hidden") ){
-		$(".gnb").removeAttr("style");
-	}
 });
