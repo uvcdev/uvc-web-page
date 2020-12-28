ad47bce2a5083ec0ddadde5023bcc806c7f7261d
hj252 <hj252@uvc.co.kr>
2020-12-28 17:50:44 +0900

    Fix header color error

--- asset/js/header_footer.js
+++ asset/js/header_footer.js
@@ -13,63 +13,57 @@ function resizeHandler() {
 
 window.addEventListener('resize', resizeHandler)
 
-$(document).ready(function() {
+$('#header').mouseenter(function(){
+	if(window.innerWidth > 1180) {
+		$(this).addClass("on");
+		$('h1 a img.color').addClass("active");
 
-	/*헤더에 마우스 올리면 클래스 토글*/
-    $('#header').mouseenter(function(){
-		if(window.innerWidth > 1180) {
-			$(this).addClass("on");
-			$('h1 a img.color').addClass("active");
-		}
-	});
-	$('#header').mouseleave(function(){
-		if(window.innerWidth > 1180) {
-			$(this).removeClass("on");
-			$('h1 a img.color').removeClass("active");
-		}
-	}); //hover기능은 로드가 전부 되지 않았을때 반대로 되는 경우가 있음, 두가지 경우도 로드 되지 않았을 때 반대로 되는 경우가 있음
-
+	}
+});
+$('#header').mouseleave(function(){
+	if(window.innerWidth > 1180) {
+		$(this).removeClass("on");
+		$('h1 a img.color').removeClass("active");
+	}
+});
 	
+$('.gnb > .sol').mouseenter(function(){
+	if(window.innerWidth > 1180){
+		$('.nav_dim').css({"height":"68px", "visibility":"visible", "opacity":"1"});
+		$('.gnb > .sol > ul').css({"visibility":"visible", "opacity":"1"});
+		$(this).addClass("active");
+	}
+});
+$('.gnb > .sol').mouseleave(function(){
+	if(window.innerWidth > 1180){
+		$('.nav_dim').css({"height":"0", "visibility":"hidden", "opacity":"0"});
+		$('.gnb > .sol > ul').css({"visibility":"hidden", "opacity":"0"});
+		$(this).removeClass("active");
+	}
+});
 
-	$('.gnb > .sol').mouseenter(function(){
-		if(window.innerWidth > 1180){
-			$('.nav_dim').css({"height":"68px", "visibility":"visible", "opacity":"1"});
-			$('.gnb > .sol > ul').css({"visibility":"visible", "opacity":"1"});
-			$(this).addClass("active");
-		}
-	});
-	$('.gnb > .sol').mouseleave(function(){
-		if(window.innerWidth > 1180){
-			$('.nav_dim').css({"height":"0", "visibility":"hidden", "opacity":"0"});
-			$('.gnb > .sol > ul').css({"visibility":"hidden", "opacity":"0"});
-			$(this).removeClass("active");
-		}
-	});
 
-	
-    /*유틸메뉴 언어선택*/
-    $('.lang').click(function(){
-		$('.lang_util ul').slideToggle(400);
-    });
-    
-	/*전체메뉴*/
-	$('.mNav').click(function(){
-		const mNav = $(this);
-		const isOn = mNav.attr('name')
-		if (isOn === 'on') {
-			$('.gnb').slideUp(400);
-			$(this).toggleClass("on");
-			mNav.attr('name', 'off')
-		} else { 
-			$('.gnb').slideDown(400);
-			$(this).toggleClass("on");
-			mNav.attr('name', 'on')
-		}
-		$('.gnb > li').click(function(){
-			$('.gnb').slideUp(400)
-			mNav.attr('name', 'off')
-			$('.mNav').removeClass("on");
-		});
-	});
+/*유틸메뉴 언어선택*/
+$('.lang').click(function(){
+	$('.lang_util ul').slideToggle(400);
+});
 
+/*전체메뉴*/
+$('.mNav').click(function(){
+	const mNav = $(this);
+	const isOn = mNav.attr('name')
+	if (isOn === 'on') {
+		$('.gnb').slideUp(400);
+		$(this).toggleClass("on");
+		mNav.attr('name', 'off')
+	} else { 
+		$('.gnb').slideDown(400);
+		$(this).toggleClass("on");
+		mNav.attr('name', 'on')
+	}
+	$('.gnb > li').click(function(){
+		$('.gnb').slideUp(400)
+		mNav.attr('name', 'off')
+		$('.mNav').removeClass("on");
+	});
 });
